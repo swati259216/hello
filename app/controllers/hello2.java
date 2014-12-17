e9c59a90f1d013702e3fad1ab03c2094ee9b496d
swati259216 <swati@zemosolabs.com>
2014-12-17 11:37:55 +0530

    Revert "commit hello 2"
    This reverts commit 8a32fe0871ba5bf56446c3d0ec0510a5649d9c71.

--- app/controllers/hello2.java
+++ app/controllers/hello2.java
@@ -7,12 +7,4 @@ public class hello2 {
 
     private String h ="hello , there";
     private int trials=3;
-
-    public String getH() {
-        return h;
-    }
-
-    public void setH(String h) {
-        this.h = h;
-    }
 }
