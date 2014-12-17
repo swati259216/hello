e36bc5b2e12fcd33f103fb89b3e3805d23be79cb
swati259216 <swati@zemosolabs.com>
2014-12-17 13:35:03 +0530

    heyyyy

--- app/controllers/hello2.java
+++ app/controllers/hello2.java
@@ -4,23 +4,21 @@ import models.Notes;
 import models.SignIn;
 import models.Users;
 import play.Logger;
-<<<<<<< HEAD
+
 import play.libs.F;
 import play.mvc.Http;
-=======
+
 import play.data.Form;
->>>>>>> tile
+
 import play.mvc.Result;
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import play.mvc.SimpleResult;
 import views.html.signup;
-=======
-=======
+
 import views.html.index;
->>>>>>> 129db90b05106ebd6ac0936892777487d052014c
+
 import views.html.signin;
->>>>>>> 7c9e38506a5241afb82442241eafe7f10723c7f9
+
 
 import java.util.List;
 
@@ -34,6 +32,10 @@ public class hello2 {
 
     private String h ="hello , there";
     private int trials=3;
+
+    public static final Form<Users> signupForm = Form.form(Users.class);
+    public static final Form<SignIn> signinForm = Form.form(SignIn.class);
+
 
 
     public String getH() {
