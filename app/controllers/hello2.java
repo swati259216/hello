6d49cffc0ab816f2a2274660ec4b453367403513
swati259216 <swati@zemosolabs.com>
2014-12-17 13:00:03 +0530

    check branch 1

--- app/controllers/hello2.java
+++ app/controllers/hello2.java
@@ -54,4 +54,11 @@ public class hello2 {
 
     }
 
+    public static Result logout(){
+        session().clear();
+        flash("success","You have been logged out!");
+        return redirect(routes.Application.signin());
+    }
+
+
 }
