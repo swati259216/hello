8a32fe0871ba5bf56446c3d0ec0510a5649d9c71
swati259216 <swati@zemosolabs.com>
2014-12-17 11:37:06 +0530

    commit hello 2

--- app/controllers/hello2.java
+++ app/controllers/hello2.java
@@ -7,4 +7,12 @@ public class hello2 {
 
     private String h ="hello , there";
     private int trials=3;
+
+    public String getH() {
+        return h;
+    }
+
+    public void setH(String h) {
+        this.h = h;
+    }
 }
